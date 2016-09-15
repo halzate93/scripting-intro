775178c4738962369c03fec7f88ae0909a4b69c7
Héctor C. Alzate <halzate93@gmail.com>
2016-09-15 11:47:59 -0500

    AI basic behaviour

---
+++ Assets/08 - AI/Scripts/MoveForward.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI
+{
+	public class MoveForward : Node
+	{
+		[SerializeField]
+		private float distance;
+
+		public override void Execute ()
+		{
+			transform.Translate (transform.forward * distance);
+		}
+	}
+}
---
+++ Assets/08 - AI/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI
+{
+	public class Rotate : Node {
+		
+		public override void Execute ()
+		{
+			transform.Rotate (0f, 90f, 0f);
+		}
+	}
+}
