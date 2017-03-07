c3cc48cbf58146a25b832cb1351352861405ca7d
Héctor C. Alzate <halzate93@gmail.com>
2017-03-07 10:39:10 -0500

    Updated position

---
+++ Assets/08 - AI/Scripts/NavMeshAgentController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI
+{
+	[RequireComponent (typeof(NavMeshAgent))]
+	public class NavMeshAgentController : MonoBehaviour 
+	{
+		[SerializeField]
+		private LayerMask walkable;
+		private NavMeshAgent agent;
+
+		private void Awake ()
+		{
+			agent = GetComponent<NavMeshAgent> ();
+		}
+
+		private void Update ()
+		{
+			if (Input.GetMouseButtonDown (0))
+			{
+				Vector3 position = GetPosition ();
+				agent.SetDestination (position);
+			}
+		}
+
+		private Vector3 GetPosition ()
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, walkable))
+				return hit.point;
+			else
+				return transform.position;
+		}
+	}
+}
