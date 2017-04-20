36734c47dfbb21ea1ba7ead7cd7f60c6ba9e84cb
Héctor C. Alzate <halzate93@gmail.com>
2017-04-20 10:57:27 -0500

    Coroutines example

---
+++ Assets/12 - Mobile/Accelerometer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mobile
+{
+	public class Accelerometer : MonoBehaviour 
+	{
+		private void Update () 
+		{
+			Debug.Log (GetAcceleration ());
+		}
+
+		private Vector3 GetAcceleration ()
+		{
+			#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE)
+				return Input.acceleration;
+			#else
+				return GetFakeAcceleration ();
+			#endif
+		}
+
+		private Vector3 GetFakeAcceleration ()
+		{
+			float x = Input.GetAxis ("Horizontal");
+			float y = Input.GetAxis ("Vertical");
+			return new Vector3 (x, y, 0f);
+		}
+
+	}
+}
---
+++ Assets/12 - Mobile/PlatformDependentCompilation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Mobile
+{
+	public class PlatformDependentCompilation : MonoBehaviour 
+	{
+		private void Start () 
+		{
+			#if UNITY_EDITOR && !UNITY_ANDROID
+			Debug.Log ("Editor");
+			#else
+			Debug.Log ("Device");
+			#endif
+		}
+	}
+}
